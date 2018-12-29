32f25b1da69fa68a994d9de224e3bf10332a062e
刘蓓 <liubei@liubeideMacBook-Pro.local>
2018-12-29 13:24:41 +0800

    Change the input

--- diamond.py
+++ diamond.py
@@ -1,7 +1,15 @@
 import sys
-a = sys.argv[1]
+#a = sys.argv[1]
+print ("please enter one number less than 50")
+a=raw_input()
+for i in range(1,100):
+    if(int(a)>50):
+        print ("please enter one number less than 50")
+        a=raw_input()
+    else:
+        break
 b=a=int(int(a)/2)+1
 for i in range (0,int(a)):
-  print (" "*(int(b)-int(i))+"*"*(int(i)*2+1))
+    print (" "*(int(b)-int(i))+"*"*(int(i)*2+1))
 for i in range (2,int(a)+1):
-  print (" "*(int(i))+"*"*((int(b)-int(i))*2+1))
+    print (" "*(int(i))+"*"*((int(b)-int(i))*2+1))
